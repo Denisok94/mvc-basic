8aa3943cb5a5562535a65034ae841af45923e6bf
Денис <59793012+Denisok94@users.noreply.github.com>
2025-08-15 00:52:10 +0300

    [*] mini upd

--- web/index.php
+++ web/index.php
@@ -2,8 +2,12 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../core/Core.php';
-$config = require __DIR__ . "/../config/web.php";
-
-(new \LiteMvc\Core\Core($config))->run();
+try {
+    $config = require __DIR__ . "/../config/web.php";
+    
+    (new \LiteMvc\Core\Core($config))->run();
+} catch (\Throwable $th) {
+    echo $th->getMessage() . "";
+}
 
 ?>
