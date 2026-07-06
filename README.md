00b9b0d55fb1a76c3bfe74997bd7a0b7b1c9bd57
Denis Popov <giorgi-gi.pov@mail.ru>
2026-07-06 22:45:59 +0300

    [*] core upd to ver 0.1.1

--- README.md
+++ README.md
@@ -6,4 +6,5 @@ composer install --no-dev --ignore-platform-reqs
 
 composer update --no-dev --ignore-platform-reqs
 
+cd ./web
 php -S localhost:8000
--- web/index.php
+++ web/index.php
@@ -1,6 +1,8 @@
 <?php
 
 // ini_set('session.save_path', 'tmp');
+session_start();
+session_write_close();
 require __DIR__ . '/../vendor/autoload.php';
 
 try {
